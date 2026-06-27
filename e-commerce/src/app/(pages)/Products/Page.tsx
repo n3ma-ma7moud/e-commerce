d29af8597a69f075f4fb9011a1ed594df8358dd5
n3ma-ma7moud <n3n3ma7moud@gmail.com>
2026-06-27 13:19:33 +0300

    feat: fetch and display products from API

--- e-commerce/src/app/(pages)/Products/Page.tsx
+++ e-commerce/src/app/(pages)/Products/Page.tsx
@@ -1,8 +1,14 @@
+import { ProductsResponse } from '@/Interfaces/productinterface';
 import React from 'react'
 
-export default function products() {
+export default async function products() {
+  const response = await fetch ('https://ecommerce.routemisr.com/api/v1/products');
+  const data : ProductsResponse = await response .json();
   return <>
   <h2>Products</h2>
+  
+
+
   
   </>
 }
